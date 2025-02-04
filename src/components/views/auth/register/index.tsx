766e46a8a37b5997b3afc3456bcfeb0a40d645c3
Andika Zulprisa Adha <andikazulprisa27@gmail.com>
2025-02-05 04:11:51 +0700

    add registerpage

---
+++ src/components/views/auth/register/index.tsx
@@ -0,0 +1,122 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { FormEvent, useState } from "react";
+
+const RegisterView = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const { push } = useRouter();
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+    setError("");
+    const form = event.target as HTMLFormElement;
+    const data = {
+      email: form.email.value,
+      fullname: form.fullname.value,
+      phone: form.phone.value,
+      password: form.password.value,
+    };
+
+    const result = await fetch("/api/user/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (result.status === 200) {
+      form.reset();
+      setIsLoading(false);
+      push("/auth/login");
+    } else {
+      setIsLoading(false);
+      setError("Email is already registered");
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-white">
+      <div className="w-full max-w-md p-6 bg-white shadow-md rounded-md">
+        <h1 className="text-2xl font-bold text-black mb-6 text-center">
+          Register
+        </h1>
+        {error && <p className="text-red-600 px-10 text-center">{error}</p>}
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-black"
+            >
+              Email
+            </label>
+            <input
+              name="email"
+              id="email"
+              type="email"
+              className="mt-1 block w-full p-2 border text-black border-gray-300 rounded-md"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="fullname"
+              className="block text-sm font-medium text-black"
+            >
+              Fullname
+            </label>
+            <input
+              name="fullname"
+              id="fullname"
+              type="text"
+              className="mt-1 block w-full p-2 border text-black border-gray-300 rounded-md"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="phone"
+              className="block text-sm font-medium text-black"
+            >
+              Phone
+            </label>
+            <input
+              name="phone"
+              id="phone"
+              type="text"
+              className="mt-1 block w-full p-2 border text-black border-gray-300 rounded-md"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-black"
+            >
+              Password
+            </label>
+            <input
+              name="password"
+              id="password"
+              type="password"
+              className="mt-1 block w-full p-2 text-black border border-gray-300 rounded-md"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-black text-white py-2 rounded-md hover:bg-gray-800 transition"
+          >
+            {isLoading ? "Loading.." : "Register"}
+          </button>
+        </form>
+        <p className="mt-4 text-sm text-black text-center">
+          Have an account?{" "}
+          <Link href="/auth/login" className="text-blue-600 hover:underline">
+            Sign in here
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterView;
---
+++ src/lib/firebase/service.ts
@@ -0,0 +1,72 @@
+import {
+  getDocs,
+  getDoc,
+  getFirestore,
+  collection,
+  query,
+  where,
+  addDoc,
+  doc,
+} from "firebase/firestore";
+import app from "./init";
+import bcrypt from "bcrypt";
+
+const firestore = getFirestore(app);
+
+export async function retrieveData(collectionName: string) {
+  const querySnapshot = await getDocs(collection(firestore, collectionName));
+  const data = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  return data;
+}
+
+export async function retrieveDataById(collectionName: string, id: string) {
+  const docRef = doc(firestore, collectionName, id);
+  const snapshot = await getDoc(docRef);
+
+  if (!snapshot.exists()) {
+    throw new Error(`Dokumen dengan ID ${id} tidak ditemukan`);
+  }
+
+  return { id: snapshot.id, ...snapshot.data() };
+}
+
+export async function signUp(
+  userData: {
+    email: string;
+    fullname: string;
+    phone: string;
+    password: string;
+    role?: string;
+  },
+  callback: (status: boolean) => void
+) {
+  try {
+    const q = query(
+      collection(firestore, "users"),
+      where("email", "==", userData.email)
+    );
+    const snapshot = await getDocs(q);
+
+    if (!userData.role) {
+      userData.role = "member";
+    }
+
+    if (!snapshot.empty) {
+      callback(false);
+    } else {
+      userData.password = await bcrypt.hash(userData.password, 10);
+
+      await addDoc(collection(firestore, "users"), userData);
+      callback(true);
+    }
+  } catch (error) {
+    console.error("Error during sign-up", error);
+    callback(false);
+  }
+}
+
+export default signUp;
