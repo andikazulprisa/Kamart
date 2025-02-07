e63c5986d17a694f30b824a95d4513258ae14f10
Andika Zulprisa Adha <andikazulprisa27@gmail.com>
2025-02-07 22:27:05 +0700

    initial push for iteration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen p-8 pb-20 sm:p-20">
       <main className="flex flex-col gap-8 items-center sm:items-start">
-        <h1 className="text-black text-5xl">MASUK DI /login YA</h1>
+        <h1 className="text-black text-5xl">KAMART | SHOP</h1>
+        <p className="text-black text-xl">
+          /home, /product, /auth/login, /auth/register{" "}
+        </p>
         <div className="flex gap-4">
           <button className="bg-black text-white px-4 py-2 rounded flex items-center gap-2">
             <FaProductHunt />
