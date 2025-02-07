31cd109e7632729fa8dda915c416d309695cd724
Andika Zulprisa Adha <andikazulprisa27@gmail.com>
2025-02-07 17:40:09 +0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen p-8 pb-20 sm:p-20">
       <main className="flex flex-col gap-8 items-center sm:items-start">
-        <h1 className="text-black text-5xl">WELCOME, KAMART</h1>
+        <h1 className="text-black text-5xl">MASUK DI /login YA</h1>
         <div className="flex gap-4">
           <button className="bg-black text-white px-4 py-2 rounded flex items-center gap-2">
             <FaProductHunt />
